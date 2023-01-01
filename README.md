4db690129d294c80f9c1634ad37f4b183f56ce90
Yasin <kurucuu@hotmail.com>
2023-01-01 15:12:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sinema rezervasyon sistemi / Project
+
+## örnek proje olarak değerlendirilmiştir.
+
+Html, Css & Javascript kullanılmıştır.
+
+[Örnek Proje] (https://www.google.com.tr)
---
+++ script.js
@@ -0,0 +1,51 @@
+const container = document.querySelector('.container');
+const count = document.getElementById('count')
+const amount = document.getElementById('amount')
+const select = document.getElementById('movie')
+const seats = document.querySelectorAll('.seat:not(.reserved)')
+
+container.addEventListener('click', function(e) {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
+        e.target.classList.toggle('selected')
+        calculateTotal()
+
+
+    }
+});
+
+select.addEventListener('change', function(e){
+    calculateTotal()
+
+})
+
+
+function calculateTotal(){
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+
+    const selectedSeatsArr = []
+    const seatsArr = []
+    selectedSeats.forEach(function(seat){
+        selectedSeatsArr.push(seat);
+    });
+    seats.forEach(function(seat){
+        seatsArr.push(seat);
+    });
+    
+    let selectedSeatIndexs = selectedSeatsArr.map(function(seat){
+        return seatsArr.indexOf(seat)
+    })
+
+    let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+    let price = select.value;
+
+
+    count.innerText = selectedSeatCount
+    amount.innerText = selectedSeatCount * select.value
+
+    saveToLocalStorage(selectedSeatIndexs)
+}
+
+function saveToLocalStorage(indexs) {
+    localStorage.setItem('selectedSeats', JSON.stringify(indexs))
+    localStorage.setItem('selectedMovieIndex', select.selectedSeatIndex);
+}
